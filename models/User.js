07a88a0ca638c8b9fc9b2162253e9b2922889409
Rydi49 <shaynechassie867@hotmail.com>
2026-04-24 23:38:46 -0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  role: {
+    type: String,
+    enum: ['rider', 'owner', 'admin'],
+    default: 'rider'
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  trips: {
+    type: Number,
+    default: 0
+  },
+  responseTime: {
+    type: String,
+    default: '< 1 hour'
+  },
+  lastLogin: {
+    type: Date
+  }
+}, {
+  timestamps: { createdAt: true, updatedAt: false }
+});
+
+userSchema.methods.toJSON = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.__v;
+  return obj;
+};
+
+module.exports = mongoose.model('User', userSchema);
